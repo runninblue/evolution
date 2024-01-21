895ce54379ce07f0b147b0bfa45825852551f5a2
runninblue <efthymios.hmichael@gmail.com>
2024-01-21 21:18:32 +0200

    Updated timeline

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { NgxSliderModule } from 'ngx-slider-v2';
+import { Options } from 'ngx-slider-v2';
 
 @Component({
   selector: 'app-timeline',
   standalone: true,
-  imports: [],
+  imports: [NgxSliderModule],
   templateUrl: './timeline.component.html',
   styleUrl: './timeline.component.scss'
 })
 export class TimelineComponent {
-
+  value: number = 100;
+  options: Options = {
+    floor: 0,
+    ceil: 200
+  }
 }
